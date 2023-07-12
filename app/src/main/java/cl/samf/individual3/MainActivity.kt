3cc8048fcd56adf68b882663b7b08f5f3b2def61
Sergio <sergiomolinaflores1@gmail.com>
2023-07-11 20:20:00 -0400

    Ejercicio 8 Mod 5, de Java se transforma a lenguaje Kotlin

--- app/src/main/java/cl/samf/individual3/MainActivity.kt
+++ app/src/main/java/cl/samf/individual3/MainActivity.kt
@@ -1,115 +1,102 @@
-package cl.samf.individual3;
+package cl.samf.individual3
 
-import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 
-import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.TextView;
-import android.widget.Toast;
-
-public class MainActivity extends AppCompatActivity {
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        Button sumar = findViewById(R.id.buttonSumar);
-        Button restar = findViewById(R.id.buttonMenos);
-        Button multip = findViewById(R.id.buttonMulti);
-        Button divid = findViewById(R.id.buttonDiv);
-        Button cerrar = findViewById(R.id.buttonCerrar);
-        EditText txtEntrada1 = findViewById(R.id.editTextNumero1);
-        EditText txtEntrada2 = findViewById(R.id.editTextNumero2);
-        TextView result = findViewById(R.id.textResultado);
-        sumar.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                String txt1 = txtEntrada1.getText().toString().trim();
-                String txt2 = txtEntrada2.getText().toString().trim();
-                if (txt1.isEmpty()) {
-                    Toast.makeText(MainActivity.this, "Ingresa un Primer Numero", Toast.LENGTH_SHORT).show();
-                }else if (txt2.isEmpty()) {
-                    Toast.makeText(MainActivity.this, "Ingresa un Segundo Numero", Toast.LENGTH_SHORT).show();
-                }else {
-                    int nro1 = Integer.parseInt(txt1);
-                    int nro2 = Integer.parseInt(txt2);
-                    int resultado = nro1 + nro2;
-                    result.setText(String.valueOf(resultado));
-                    txtEntrada1.setText("");
-                    txtEntrada2.setText("");
-                }
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val sumar = findViewById<Button>(R.id.buttonSumar)
+        val restar = findViewById<Button>(R.id.buttonMenos)
+        val multip = findViewById<Button>(R.id.buttonMulti)
+        val divid = findViewById<Button>(R.id.buttonDiv)
+        val cerrar = findViewById<Button>(R.id.buttonCerrar)
+        val txtEntrada1 = findViewById<EditText>(R.id.editTextNumero1)
+        val txtEntrada2 = findViewById<EditText>(R.id.editTextNumero2)
+        val result = findViewById<TextView>(R.id.textResultado)
+        sumar.setOnClickListener {
+            val txt1 = txtEntrada1.text.toString().trim { it <= ' ' }
+            val txt2 = txtEntrada2.text.toString().trim { it <= ' ' }
+            if (txt1.isEmpty()) {
+                Toast.makeText(this@MainActivity, "Ingresa un Primer Numero", Toast.LENGTH_SHORT)
+                    .show()
+            } else if (txt2.isEmpty()) {
+                Toast.makeText(this@MainActivity, "Ingresa un Segundo Numero", Toast.LENGTH_SHORT)
+                    .show()
+            } else {
+                val nro1 = txt1.toInt()
+                val nro2 = txt2.toInt()
+                val resultado = nro1 + nro2
+                result.text = resultado.toString()
+                txtEntrada1.setText("")
+                txtEntrada2.setText("")
             }
-        });
-
-        restar.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                String txt1 = txtEntrada1.getText().toString().trim();
-                String txt2 = txtEntrada2.getText().toString().trim();
-                if (txt1.isEmpty()) {
-                    Toast.makeText(MainActivity.this, "Ingresa un Primer Numero", Toast.LENGTH_SHORT).show();
-                }else if (txt2.isEmpty()) {
-                    Toast.makeText(MainActivity.this, "Ingresa un Segundo Numero", Toast.LENGTH_SHORT).show();
-                }else {
-                    int nro1 = Integer.parseInt(txt1);
-                    int nro2 = Integer.parseInt(txt2);
-                    int resultado = nro1 - nro2;
-                    result.setText(String.valueOf(resultado));
-                    txtEntrada1.setText("");
-                    txtEntrada2.setText("");
-                }
+        }
+        restar.setOnClickListener {
+            val txt1 = txtEntrada1.text.toString().trim { it <= ' ' }
+            val txt2 = txtEntrada2.text.toString().trim { it <= ' ' }
+            if (txt1.isEmpty()) {
+                Toast.makeText(this@MainActivity, "Ingresa un Primer Numero", Toast.LENGTH_SHORT)
+                    .show()
+            } else if (txt2.isEmpty()) {
+                Toast.makeText(this@MainActivity, "Ingresa un Segundo Numero", Toast.LENGTH_SHORT)
+                    .show()
+            } else {
+                val nro1 = txt1.toInt()
+                val nro2 = txt2.toInt()
+                val resultado = nro1 - nro2
+                result.text = resultado.toString()
+                txtEntrada1.setText("")
+                txtEntrada2.setText("")
             }
-        });
-
-        multip.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                String txt1 = txtEntrada1.getText().toString().trim();
-                String txt2 = txtEntrada2.getText().toString().trim();
-                if (txt1.isEmpty()) {
-                    Toast.makeText(MainActivity.this, "Ingresa un Primer Numero", Toast.LENGTH_SHORT).show();
-                }else if (txt2.isEmpty()) {
-                    Toast.makeText(MainActivity.this, "Ingresa un Segundo Numero", Toast.LENGTH_SHORT).show();
-                }else {
-                    int nro1 = Integer.parseInt(txt1);
-                    int nro2 = Integer.parseInt(txt2);
-                    int resultado = nro1 * nro2;
-                    result.setText(String.valueOf(resultado));
-                    txtEntrada1.setText("");
-                    txtEntrada2.setText("");
-                }
+        }
+        multip.setOnClickListener {
+            val txt1 = txtEntrada1.text.toString().trim { it <= ' ' }
+            val txt2 = txtEntrada2.text.toString().trim { it <= ' ' }
+            if (txt1.isEmpty()) {
+                Toast.makeText(this@MainActivity, "Ingresa un Primer Numero", Toast.LENGTH_SHORT)
+                    .show()
+            } else if (txt2.isEmpty()) {
+                Toast.makeText(this@MainActivity, "Ingresa un Segundo Numero", Toast.LENGTH_SHORT)
+                    .show()
+            } else {
+                val nro1 = txt1.toInt()
+                val nro2 = txt2.toInt()
+                val resultado = nro1 * nro2
+                result.text = resultado.toString()
+                txtEntrada1.setText("")
+                txtEntrada2.setText("")
             }
-        });
-
-        divid.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                String txt1 = txtEntrada1.getText().toString().trim();
-                String txt2 = txtEntrada2.getText().toString().trim();
-                if (txt1.isEmpty()) {
-                    Toast.makeText(MainActivity.this, "Ingresa un Primer Numero", Toast.LENGTH_SHORT).show();
-                }else if (txt2.isEmpty()) {
-                    Toast.makeText(MainActivity.this, "Ingresa un Segundo Numero", Toast.LENGTH_SHORT).show();
-                } else if (txt2.equals("0")) {
-                    Toast.makeText(MainActivity.this, "El numero dos debe ser distinto a 0", Toast.LENGTH_SHORT).show();
-                }else {
-                    int nro1 = Integer.parseInt(txt1);
-                    int nro2 = Integer.parseInt(txt2);
-                    int resultado = nro1 / nro2;
-                    result.setText(String.valueOf(resultado));
-                    txtEntrada1.setText("");
-                    txtEntrada2.setText("");
-                }
+        }
+        divid.setOnClickListener {
+            val txt1 = txtEntrada1.text.toString().trim { it <= ' ' }
+            val txt2 = txtEntrada2.text.toString().trim { it <= ' ' }
+            if (txt1.isEmpty()) {
+                Toast.makeText(this@MainActivity, "Ingresa un Primer Numero", Toast.LENGTH_SHORT)
+                    .show()
+            } else if (txt2.isEmpty()) {
+                Toast.makeText(this@MainActivity, "Ingresa un Segundo Numero", Toast.LENGTH_SHORT)
+                    .show()
+            } else if (txt2 == "0") {
+                Toast.makeText(
+                    this@MainActivity,
+                    "El numero dos debe ser distinto a 0",
+                    Toast.LENGTH_SHORT
+                ).show()
+            } else {
+                val nro1 = txt1.toInt()
+                val nro2 = txt2.toInt()
+                val resultado = nro1 / nro2
+                result.text = resultado.toString()
+                txtEntrada1.setText("")
+                txtEntrada2.setText("")
             }
-        });
-        cerrar.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                System.exit(0);
-            }
-        });
+        }
+        cerrar.setOnClickListener { finish() }
     }
 }
